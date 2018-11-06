18da33445202ebdfceb2f668054437e36ea709bc
Joao andre FARINHA CARRASCO RAMOS <jfarinha@e1r2p10.42.fr>
2018-11-06 15:54:42 +0100

    added vectop function to utils

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,11 +6,12 @@
 /*   By: jfarinha <jfarinha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 19:09:29 by jfarinha          #+#    #+#             */
-/*   Updated: 2018/11/04 19:30:18 by jfarinha         ###   ########.fr       */
+/*   Updated: 2018/11/06 15:52:47 by jfarinha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
+#include <stdio.h>
 
 int		on_screen(t_point pt)
 {
@@ -29,4 +30,13 @@ int		mkcolor(t_uchar r, t_uchar g, t_uchar b)
 	color = (color << 8) + g;
 	color = (color << 8) + b;
 	return (color);
+}
+
+t_point	vectop(t_vector3f vector, t_vector3f cam)
+{
+	t_point	p;
+
+	p.x = (vector.x - cam.x) * ((vector.z - cam.z) / vector.z) + cam.x;
+	p.y = (vector.y - cam.y) * ((vector.z - cam.z) / vector.z) + cam.y;
+	return (p);
 }
